a825b2d323749d41f2abff9de39f7d0795215602
XiaoLei123-xxl <381959437@qq.com>
2022-08-24 14:59:07 +0800

    第一次提交

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("hello world");
+
+        //很简单的循环
+        int i = 0;
+        for (; i < 11; i++) {
+            System.out.println("i=" + i);
+        }
+        System.out.println("i结束=" + i);
+
+    }
+}
